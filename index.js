74b017654430c5372505c55263d6eab14cdd3fab
einnad <146109295+einnad@users.noreply.github.com.>
2024-01-11 16:50:44 +0000

    Complete random gen feature with axios/api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
@@ -13,4 +14,18 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/submit", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.openbrewerydb.org/v1/breweries/random"
+    );
+    const result = response.data;
+    console.log(result[0]);
+    res.render("index.ejs", { random: result[0] });
+  } catch (error) {
+    console.log(error);
+    // res.status(404).send(error.message);
+  }
 });
